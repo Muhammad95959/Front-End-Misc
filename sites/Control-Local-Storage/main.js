6b09a445da477b4a131ca67d8a383d6a8d5975d6
muhammadhefzey@gmail.com <muhammadhefzey@gmail.com>
2024-11-27 14:59:04 +0200

    add Control-Local-Storage

---
+++ sites/Control-Local-Storage/main.js
@@ -0,0 +1,45 @@
+const input = document.querySelector("input");
+const checkBtn = document.querySelector(".check");
+const addBtn = document.querySelector(".add");
+const deleteBtn = document.querySelector(".delete");
+const showBtn = document.querySelector(".show");
+const results = document.querySelector(".results");
+
+checkBtn.addEventListener("click", function () {
+  const item = input.value;
+  if (item.length !== 0) {
+    if (window.localStorage.getItem(item) !== null)
+      results.innerHTML = `Found Local Storage Item Called <span>${item}</span>`;
+    else
+      results.innerHTML = `No Local Storage Item With The Name <span>${item}</span>`;
+  }
+});
+
+addBtn.addEventListener("click", function () {
+  const item = input.value;
+  if (item.length !== 0) {
+    window.localStorage.setItem(item, item);
+    results.innerHTML = `Local Storage Item <span>${item}</span> Added`;
+  }
+});
+
+deleteBtn.addEventListener("click", function () {
+  const item = input.value;
+  if (item.length !== 0) {
+    if (window.localStorage.getItem(item) !== null) {
+      window.localStorage.removeItem(item);
+      results.innerHTML = `Local Storage Item <span>${item}</span> Deleted`;
+    } else
+      results.innerHTML = `No Local Storage Item With The Name <span>${item}</span>`;
+  }
+});
+
+showBtn.addEventListener("click", function () {
+  const items = Object.keys(window.localStorage);
+  results.innerHTML = "";
+  for (let i = 0; i < items.length; i++) {
+    const p = document.createElement("p");
+    p.textContent = items[i];
+    results.appendChild(p);
+  }
+});
